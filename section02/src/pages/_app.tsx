c768be1829d63ae4e639afb64fc4651c9eea3171
paulsea27115 <csb070115@gmail.com>
2024-09-12 20:58:52 +0900

    feat:section02-07 global-layout

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,37 +1,11 @@
-import Link from "next/link";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import GlobalsLayout from "@/components/global-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test");
-    // push : 해당 페이지로 이동
-    // replace : 뒤로가기 막고 이동
-    // back : 전 페이지로 이동
-  };
-
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={"/"}>index</Link>
-        &nbsp;
-        <Link href={"/search"} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={"/book/1"}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
+    <GlobalsLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalsLayout>
   );
 }
