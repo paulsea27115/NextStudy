1f5b7a98933fdecdfec13e582c397734ab2b0eba
paulsea27115 <csb070115@gmail.com>
2024-09-19 18:46:50 +0900

    feat: section02-09

--- section02/README 2.md
+++
@@ -1,1 +0,0 @@
-# section02 space
---
+++ section02/src/components/book-item.tsx
@@ -0,0 +1,26 @@
+import type { BookData } from "@/types";
+import Link from "next/link";
+import style from "./book-time.module.css";
+
+export default function BookItem({
+  id,
+  title,
+  subTitle,
+  description,
+  author,
+  publisher,
+  coverImgUrl,
+}: BookData) {
+  return (
+    <Link href={`/book/${id}`} className={style.container}>
+      <img src={coverImgUrl} alt="book_img" />
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
